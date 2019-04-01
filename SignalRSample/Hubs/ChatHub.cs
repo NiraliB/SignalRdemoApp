838b22866a04e7524302ef0df657466e6875791d
Nirali Savani <niralisavani@promactinfo.com>
2019-04-01 12:24:19 +0530

    SignalR Demo Application

---
+++ SignalRSample/Hubs/ChatHub.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRSample.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRSample.Hubs
+{
+    public class ChatHub : Hub<IChatClient>
+    {
+        public async Task SendMessagge(string user, string message)
+        {
+            await Clients.All.ReceiveMessage( user, message);
+        }
+
+        public async Task SendMessageToCaller(string message)
+        {
+            await Clients.Caller.ReceiveMessage(message);
+        }
+
+        public async Task SendMessageToGroup(string message)
+        {
+            await Clients.Group("SignalUsers").ReceiveMessage(message);
+        }
+
+        [HubMethodName("SendMessageToUser")]
+        public Task DirectMessage(string user, string message)
+        {
+            return Clients.User(user).ReceiveMessage(message);
+        }
+
+        public Task ThrowException()
+        {
+            throw new HubException("This error will be sent to the client!");
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "SignalUsers");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "SignalUsers");
+            await base.OnDisconnectedAsync(exception);
+        }
+
+
+
+    }
+}
--- SignalRSample/Startup.cs
+++ SignalRSample/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SignalRSample.Hubs;
 
 namespace SignalRSample
 {
@@ -31,8 +32,9 @@ namespace SignalRSample
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +53,11 @@ namespace SignalRSample
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chatHub");
+            });
 
             app.UseMvc();
         }
